c43c03dd9db3d2ce750e5049aa434f8317d4b4d8
cashinout422 <63626066+cashinout422@users.noreply.github.com>
2022-01-15 03:58:40 +0000

    Add files via upload

--- monke-AI.py
+++ monke-AI.py
@@ -1,3 +1,12 @@
 name = input("what is your monke name? ")
+source = input("do you like monke? ")
+monke = input("what do you like? ")
 
-print("hello monke " + name)
+if source != "yes":
+    print("get out!")
+    quit()
+if source != "no":
+    print("good monke")
+if monke != "monke":
+    print("worng!")
+    quit()
